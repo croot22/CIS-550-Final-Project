14fc7d3c5b304a4fe95823c25437ea549df9aa5d
JasmineYJ <49506538+JasmineYJ@users.noreply.github.com>
2021-04-15 02:02:00 -0700

    added yelp signature
    routes.js

--- server/routes.js
+++ server/routes.js
@@ -76,6 +76,95 @@ function bestGenresPerDecade(req, res) {
 };
 
 
+// Yelp 1 of 2 - recommend restaurant based on category and check in 
+
+function bestRestaurant(req, res) {
+  if(!req.query.Type) {                         //look for #decade# in other file 
+    res.status(400).json({
+      'message': 'Incorrect query parameters passed.'
+    })
+  }
+  var restaurant = parseInt(req.query.restaurant);  //look for #decade# in other file 
+  var weekday = parseInt(req.query.weekday);    //look for #decade# in other file 
+  var hour = parseInt(req.query.hour);          //look for #decade# in other file 
+  var zipcode = parseInt(req.query.zipcode);    //look for #decade# in other file 
+
+  var query = 
+  `WITH BUS AS (
+    SELECT business_name, address, business_id, review_count, stars, hours
+    FROM yelp_business 
+    WHERE zipcode = ${zipcode} AND review_count > 30 AND business_id IN
+      (SELECT business_id
+      FROM yelp_categories
+      WHERE category = ${restaurant}) 
+    ORDER BY stars DESC
+    LIMIT 100
+  ), op AS (
+    SELECT business_id, weekday, hour, count(*)
+    FROM yelp_checkin
+    WHERE weekday = ${weekday} AND hour = ${hour}
+    GROUP by business_id, weekday, hour
+    HAVING count(*) > 2
+  )
+  SELECT business_name, address, stars, hours, review_content as top_review
+  FROM 
+    (SELECT business_name, address, review_content, BUS.stars, hours,
+             ROW_NUMBER() OVER (PARTITION BY business_name
+                              ORDER BY (useful+funny+cool) DESC
+                             ) AS rn
+    FROM yelp_review YR JOIN BUS ON YR.business_id = BUS.business_id JOIN op ON YR.business_id = op.business_id
+    ) temp
+  WHERE rn <= 1
+  ORDER BY stars DESC LIMIT 3`; 
+
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
+
+// Yelp 2 of 2 - recommend based on category and check in 
+
+function bestPlace(req, res) {
+  if(!req.query.Type) {                         //look for #decade# in other file 
+    res.status(400).json({
+      'message': 'Incorrect query parameters passed.'
+    })
+  }
+  
+  var weekday = parseInt(req.query.weekday);    //look for #decade# in other file 
+  var hour = parseInt(req.query.hour);          //look for #decade# in other file 
+  var zipcode = parseInt(req.query.zipcode);    //look for #decade# in other file 
+
+  var query = 
+  `WITH BUS AS (
+      SELECT business_name, address, business_id, review_count, stars, hours
+      FROM yelp_business 
+      WHERE zipcode = ${zipcode} 
+    ), op AS (
+      SELECT business_id, weekday, hour, count(*) as volume1
+      FROM yelp_checkin
+      WHERE weekday = ${weekday} AND hour = ${hour}
+      GROUP by business_id, weekday, hour
+    )
+
+  SELECT category, SUM(volume1) as volume
+  FROM yelp_categories cat JOIN BUS ON cat.business_id = BUS.business_id JOIN op ON cat.business_id = op.business_id
+  GROUP by category
+  ORDER BY volume DESC 
+  LIMIT 3
+  `;
+
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
+
 //OLD BELOW:
 
 function getAllGenres(req, res) {
